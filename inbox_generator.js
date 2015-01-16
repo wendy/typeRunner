342b582cb0e9cb5362305e7cc14636b6ac002e1d
Wendy Leung <wendysarahleung@gmail.com>
2015-01-16 13:26:07 -0800

    2nd draft

---
+++ inbox_generator.js
@@ -0,0 +1,6 @@
+//utility function
+var randomElement = function(array){
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}; 
+
--- script.js
+++ script.js
@@ -1,39 +1,61 @@
 $(document).ready(function() {
   $txtInput = $('.txtInput');
-  var index = 0;
-  var array = $('.currentDiv').text().split(' ');
-  var enterSign = array.shift();
-  console.log(array);
-  
-  for(var i = 0; i < array.length; i++) {
-    var lastChar = array[i].length - 1;
-    if(array[i] === "") {
-      array.splice(i, 1);
-      i -= 1;
-    } else if (array[i][lastChar] === enterSign) {
-      array[i] = array[i].slice(0, lastChar);
-    }  
-  };
-  console.log(array);
+  var index = 1;
 
+  var makePromptElement = function(array) {
+    var promptStr = '';
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] !== '<p>' && array[i] !== '</p>') {
+        promptStr += '&nbsp;' + '<span>' + array[i] + '</span>';
+      } else {
+        promptStr += array[i];
+      }
+    }
+    return $.parseHTML(promptStr);
+  };
+
+  //each definition
+  var each = ["<p> var each = function(collection, iterator) { </p> <p>",
+    "if (Array.isArray(collection)) { </p> <p>",
+      "for (var i = 0; i < collection.length; i++) { </p> <p>",
+        "iterator(collection[i], i, collection); </p> <p>",
+      "} </p> <p>",
+    "} else if (typeof collection === 'object') { </p> <p>",
+      "for (var key in collection) { </p> <p>",
+        "iterator(collection[key], key, collection); </p> <p>",
+      "} </p> <p>",
+    "} </p> <p>",
+  "}; </p>"].join(' ').split(' ');
+  
+
+  //making the htmlDiv and appending it
+  var htmlDiv = makePromptElement(each);
+  $('.currentDiv').append(htmlDiv);
+
+
+  //Input box 
   $txtInput.on('keypress',function(e) {
-    var target = array[index];
-    var $target = $('.currentDiv');
+    var target = each[index];
+    var nextTarget = each[index + 1];
+
+    var correct = function(extra) {
+      $txtInput.val('');
+      $('span:nth-child(' + (index + 1) + ')').removeClass('redBg');
+      index += 1 + extra;
+      $('span:nth-child(' + (index + 1) + ')').addClass('redBg');
+    };
 
     var message = $txtInput.val();
-    if(e.keyCode == 32){
+    if (e.keyCode == 32) {
       e.preventDefault()
-      if(message === target && target[0] !== '{' && target[0] !== '}' && target[-1] !== ';') {
-        $txtInput.val('');
-        index += 1;
+      if (message === target && nextTarget !== "</p>") {
+        correct(0);
       }
     } else if(e.keyCode == 13){
       e.preventDefault()
-      if(message === target && (target[0] === '{' || target[0] === '}' || target[-1] === ';')) {
-        $txtInput.val('');
-        index += 1;
+      if(nextTarget === "<br>" && message === target) {
+        correct(2);
       }
     }
-
   });
 });
