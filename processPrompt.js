e549aa439809254f5a9c0e71548eb4619448a548
Ray Ma <rhm262@gmail.com>
2015-01-16 17:18:59 -0800

    Add script to process prompt

---
+++ processPrompt.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var fileName = 'prompt1.js';
+
+var isWord = function (word) {
+    return word[0] !== ' ';
+};
+
+var wrapWord = function (word) {
+    if (word[word.length-1] === '\n') {
+        return '<span class="word">' + word.slice(0, word.length-1) + '</span>\n';
+    }
+
+    return '<span class="word">' + word.trim() + '</span>';
+};
+
+fs.readFile(fileName, 'utf8', function (error, data) {
+    var newStr = '';
+    var word = '';
+    var wordRegex = /\S/;
+    for (var i = 0; i < data.length; i++) {
+        if (wordRegex.test(data[i])) {
+            word += data[i];
+        } else {
+            if (word === '') {
+                newStr += data[i];
+            } else {
+                if (data[i] === '\n') {
+                    newStr += '<span class="word new-line">' + word + '</span>' + data[i];
+                } else {
+                    newStr += '<span class="word">' + word + '</span>' + data[i];
+                }
+                word = '';
+            }
+        }
+    }
+
+    fs.writeFile('prompt1-processed.js', newStr);
+});
--- script.js
+++ script.js
@@ -1,77 +1,36 @@
 $(document).ready(function() {
   $txtInput = $('.txtInput');
   var arrayIndex = 0;
-  var spanIndex = 0;
 
- var makePromptElement = function(array) {
-    var newArray = [];
-    var index = 1;
-    for (var i = 0; i < array.length; i++) {
-      newArray.push('<p class="p' + index + '"> ');
-      var tempArray = array[i].split(' ');
-      for (var j = 0; j < tempArray.length; j++) {
-        if (j === tempArray.length - 1) {
-          newArray.push(tempArray[j]);
-        } else {
-          newArray.push('<span>' + tempArray[j] + '</span>');
-        }      
-      }
-      var lastLetter = array[i][array[i].length - 6]
-      if (lastLetter === '{') { 
-        index += 1;
-      } else if (lastLetter === ';' || array[i][0] === '}') {
-        if (array[i + 1] === undefined) {
-        } else if (array[i + 1][0] === '}') {
-          index -= 1;
-        }
-      }  
-    }
-    return $.parseHTML(newArray.join(' '));
-  }
-
-  //each definition
-  var each = ["var each = function(collection, iterator) { </p>",
-    "if (Array.isArray(collection)) { </p>",
-      "for (var i = 0; i < collection.length; i++) { </p>",
-        "iterator(collection[i], i, collection); </p>",
-      "} </p>",
-    "} else if (typeof collection === 'object') { </p>",
-      "for (var key in collection) { </p>",
-        "iterator(collection[key], key, collection); </p>",
-      "} </p>",
-    "} </p>",
-  "}; </p>"];
   
-
+  var $words = $('.word');
   //making the htmlDiv and appending it
-  var htmlDiv = makePromptElement(each);
-  $('.currentDiv').append(htmlDiv);
-  $($('span')[0]).addClass('redBg');
-  var eachArray = each.join(' ').split(' ')
+  // var htmlDiv = makePromptElement(each);
+  // $('.currentDiv').append(htmlDiv);
+  $($words[0]).addClass('redBg');
+
   
   //Input box 
   $txtInput.on('keypress',function(e) {
-    var target = eachArray[arrayIndex];
-    var nextTarget = eachArray[arrayIndex + 1];
+    var target = $($words[arrayIndex]);
 
-    var correct = function(extra) {
+    var correct = function() {
       $txtInput.val('');
-      $($('span')[spanIndex]).removeClass('redBg');
-      arrayIndex += 1 + extra;
-      spanIndex += 1;
-      $($('span')[spanIndex]).addClass('redBg');
+      target.removeClass('redBg');
+      arrayIndex += 1;
+      $($words[arrayIndex]).addClass('redBg');
     };
 
     var message = $txtInput.val();
     if (e.keyCode == 32) {
-      e.preventDefault()
-      if (message === target && nextTarget !== "</p>") {
-        correct(0);
+      e.preventDefault();
+      if (message === target.text() && !target.hasClass('new-line')) {
+        correct();
       }
     } else if(e.keyCode == 13){
-      e.preventDefault()
-      if(nextTarget === "</p>" && message === target) {
-        correct(1);
+      e.preventDefault();
+      if(message === target.text() && target.hasClass('new-line')) {
+        correct();
       }
     }
 
